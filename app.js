170c07a06278ce1b2a985c9fa33a0484b81d14a3
Deepesh upadhayay <41473554+du0079@users.noreply.github.com>
2019-11-23 04:30:05 +0530

    modifications

--- app.js
+++ app.js
@@ -4,20 +4,21 @@ var request= require("request")
 app.set("view engine","ejs");
 
 app.use(express.static("views"));
-app.get("/",function(req,res){
-	res.send("Tested OK");
+app.get('/', function(req, res){
+    res.render('search');
 });
 
-app.get("/movies",function(req,res){
-    request("http://www.omdbapi.com/?s=california&apikey=thewdb",function(error,response,body){
-    	if(!error && response.statusCode == 200){
-    	 console.log(typeof body);
-        res.send(body);
-    	}
+app.get('/results', function(req, res){
+    var query = req.query.search;
+    var url = 'https://www.omdbapi.com/?s=' + query + '&apikey=YOUR_API_KEY';
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var data = JSON.parse(body)
+            res.render('results', {data: data});
+        }
     });
-	 // res.render("request",{});
-	 // res.send("koi hai")
-});
+}); 
+
 
  app.listen(3000, function() { 
   console.log('Server listening on port 3000'); 
